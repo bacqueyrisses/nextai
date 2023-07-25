3801dd9f04cabf1b2f6edb33744d1aa14538cb80
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2023-07-25 22:52:14 +0200

    Add header

---
+++ components/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <div className={"flex justify-between"}>
+      <div>NextAI</div>
+      <div>Check the doc</div>
+    </div>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import { SearchDialog } from '@/components/SearchDialog'
+import Header from '@/components/Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={"bg-grainy bg-[#1f2122] min-h-screen flex flex-col items-center justify-between p-6"}>
+      <main className={"bg-grainy bg-[#1f2122] min-h-screen flex flex-col justify-between p-6"}>
+        <Header/>
         <div>
           <SearchDialog />
         </div>
