5a5f2972803402f194ef193f51313e323350c849
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2023-07-26 14:42:30 +0200

    Update h1/h2

--- app/page.tsx
+++ app/page.tsx
@@ -17,11 +17,11 @@ export default function Home() {
       <main className={"bg-grainy bg-[#1f2122] min-h-screen flex flex-col justify-between px-40 py-12"}>
         <Header/>
         <section className={"flex items-center justify-between"}>
-          <div className={"text-7xl text-white basis-2/3"}>Revolutionize Your Language Abilities with Mindmate</div>
-          <div className={"text-3xl text-white basis-1/3 place-self-end flex flex-col gap-4 justify-end items-end"}>
-            <div>App Router</div>
-            <div>Pages Router</div>
-          </div>
+          {/*<div className={"text-7xl text-white basis-2/3"}>Revolutionize Your Language Abilities with Mindmate</div>*/}
+          {/*<div className={"text-3xl text-white basis-1/3 place-self-end flex flex-col gap-4 justify-end items-end"}>*/}
+          {/*  <div>App Router</div>*/}
+          {/*  <div>Pages Router</div>*/}
+          {/*</div>*/}
         </section>
         <section>
           <SearchDialog />
