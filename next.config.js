9ab13d99b33c55aa0f6ac38db163dba3583d25b3
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2023-07-28 17:17:21 +0200

    Update next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.module.rules.push({
+        test: /YOUR FOLDER NAME\/.*/,
+        loader: 'ignore-loader',
+      })
+    }
+  },
+
 }
 
 module.exports = nextConfig
