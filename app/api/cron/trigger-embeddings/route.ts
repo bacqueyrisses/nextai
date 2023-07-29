c26ce2e85c8e2f51219bb7f6138bb6902c4f05f8
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2023-07-29 17:12:34 +0200

    Update CRON logic

--- app/api/cron/trigger-embeddings/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server'
-import axios from "axios";
-import dotenv from 'dotenv'
-dotenv.config()
-
-export async function GET() {
-  await axios.get(`${process.env.NEXT_PUBLIC_URL}/api/generate-embeddings`)
-  return NextResponse.json({ refreshed: true, now: Date.now() })
-}
