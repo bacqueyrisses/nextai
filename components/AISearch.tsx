876859514dfe68f0c8319086062314d2a42aafb1
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2023-07-30 21:01:16 +0200

    Add AISearch component interface

--- components/AISearch.tsx
+++ components/AISearch.tsx
@@ -1,4 +1,4 @@
-"use client"
+'use client'
 
 import remarkGfm from 'remark-gfm'
 import ReactMarkdown from 'react-markdown'
@@ -6,35 +6,39 @@ import { markdownComponents } from '@/components/ui/components/markdown'
 import { User, Wand } from 'lucide-react'
 import * as React from 'react'
 
-export default function AISearch({message}) {
+interface AISearchI {
+  message: string
+}
+
+export default function AISearch({ message }: AISearchI) {
   return (
-    <div key={12} className="px-4 [overflow-anchor:none] mb-6">
-      <div className="flex gap-6 [overflow-anchor:none] mb-6">
+    <div key={12} className='px-4 [overflow-anchor:none] mb-6'>
+      <div className='flex gap-6 [overflow-anchor:none] mb-6'>
         {/*<AiIconChat />*/}
         <>
 
-                 <span className="bg-emerald-500 p-2 w-8 h-8 rounded-full text-center flex items-center justify-center">
-                    <Wand width={18} className="text-white" />
+                 <span className='bg-emerald-500 p-2 w-8 h-8 rounded-full text-center flex items-center justify-center'>
+                    <Wand width={18} className='text-white' />
                   </span>
-            <ReactMarkdown
-              remarkPlugins={[remarkGfm]}
-              components={markdownComponents}
-              linkTarget="_blank"
-              className="prose dark:prose-dark space-y-4"
-              // transformLinkUri={(href) => {
-              //   console.log(href)
-              //   const supabaseUrl = new URL('www.bacqueyrisses.dev')
-              //   const linkUrl = new URL(href, 'https://www.bacqueyrisses.dev')
-              //
-              //   if (linkUrl.origin === supabaseUrl.origin) {
-              //     return linkUrl.toString()
-              //   }
-              //
-              //   return "www.bacqueyrisses.dev"
-              // }}
-            >
-              {message}
-            </ReactMarkdown>
+          <ReactMarkdown
+            remarkPlugins={[remarkGfm]}
+            components={markdownComponents}
+            linkTarget='_blank'
+            className='prose dark:prose-dark space-y-4'
+            // transformLinkUri={(href) => {
+            //   console.log(href)
+            //   const supabaseUrl = new URL('www.bacqueyrisses.dev')
+            //   const linkUrl = new URL(href, 'https://www.bacqueyrisses.dev')
+            //
+            //   if (linkUrl.origin === supabaseUrl.origin) {
+            //     return linkUrl.toString()
+            //   }
+            //
+            //   return "www.bacqueyrisses.dev"
+            // }}
+          >
+            {message}
+          </ReactMarkdown>
         </>
       </div>
     </div>
