044f299d4e2d3c8490eb6c94611c5ee2627032c8
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2023-07-31 20:00:30 +0200

    Update Layout TS

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,19 @@
 import '@/styles/tailwind.css'
-import { RootLayout } from '@/components/RootLayout'
+import {RootLayout} from '@/components/RootLayout'
 
 export const metadata = {
-  title: {
-    template: 'NextAI',
-    default: 'Meta',
-  },
+    title: {
+        template: 'NextAI',
+        default: 'Meta',
+    },
 }
 
-export default function Layout({ children }) {
-  return (
-    <html lang="en" className="h-full bg-neutral-950 text-base antialiased">
-      <body className="flex min-h-full flex-col">
+export default function Layout({children}: { children: React.ReactNode }) {
+    return (
+        <html lang="en" className="h-full bg-neutral-950 text-base antialiased">
+        <body className="flex min-h-full flex-col">
         <RootLayout>{children}</RootLayout>
-      </body>
-    </html>
-  )
+        </body>
+        </html>
+    )
 }
