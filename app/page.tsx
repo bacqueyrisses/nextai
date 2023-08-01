aaa57762b5563eef6f9602389d50b332c48fd464
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2023-08-01 18:45:49 +0200

    Update h1

--- app/page.tsx
+++ app/page.tsx
@@ -42,12 +42,8 @@ export default async function Home() {
       <Container className="mt-10 sm:mt-20">
         <div className="max-w-3xl">
           <h1 className="font-display text-3xl font-medium tracking-tight text-neutral-950 [text-wrap:balance] sm:text-7xl">
-            The AI-Powered
-            <br />
-            Next.JS
-            <span className={'hidden sm:inline '}> Documentation</span>
-            <span className={'inline sm:hidden'}> Doc</span>
-            <br />
+            The AI-Powered <br />{' '}
+            <span className={'whitespace-nowrap'}>Next.JS Documentation</span>
           </h1>
           <p className="mt-4 flex flex-col text-lg text-neutral-600 sm:flex-row sm:gap-1 sm:text-xl">
             <span>
