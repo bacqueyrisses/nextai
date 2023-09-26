54881a8179f9ea3db65f09b73aa0c2d924a36243
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2023-09-26 18:26:52 +0200

    Update meta-title & add OG image

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import '@/styles/globals.css'
 import RootLayout from '@/components/RootLayout'
 
 export const metadata = {
-  title: 'NextAI',
+  title: '✨NextAI',
   description: 'The Next.JS AI Doc Search',
 }
 
