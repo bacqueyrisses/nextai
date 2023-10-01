4c0cf989eaa3aea951abd39bca1a1e1c7e2db4a9
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2023-10-01 19:25:55 +0200

    Update meta-title

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import '@/styles/globals.css'
 import RootLayout from '@/components/RootLayout'
 
 export const metadata = {
-  title: '✨ NextAI',
+  title: 'NextAI',
   description: 'The Next.JS AI Doc Search',
 }
 
