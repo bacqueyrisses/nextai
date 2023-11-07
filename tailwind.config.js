026b6126291b5dec7e24838c30f23cce0dc6d4f3
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2023-11-07 14:46:45 +0800

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,44 +2,41 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        './app/**/*.{js,ts,jsx,tsx}',
-        './pages/**/*.{js,ts,jsx,tsx}',
-        './components/**/*.{js,ts,jsx,tsx}',
-
-        // Or if using `src` directory:
-        './src/**/*.{js,ts,jsx,tsx}',
-    ],
-    theme: {
-        fontSize: {
-            xs: ['0.75rem', {lineHeight: '1rem'}],
-            sm: ['0.875rem', {lineHeight: '1.5rem'}],
-            base: ['1rem', {lineHeight: '1.75rem'}],
-            lg: ['1.125rem', {lineHeight: '1.75rem'}],
-            xl: ['1.25rem', {lineHeight: '2rem'}],
-            '2xl': ['1.5rem', {lineHeight: '2.25rem'}],
-            '3xl': ['1.75rem', {lineHeight: '2.25rem'}],
-            '4xl': ['2rem', {lineHeight: '2.5rem'}],
-            '5xl': ['2.5rem', {lineHeight: '3rem'}],
-            '6xl': ['3rem', {lineHeight: '3.5rem'}],
-            '7xl': ['4rem', {lineHeight: '4.5rem'}],
-        },
-        extend: {
-            borderRadius: {
-                '4xl': '2.5rem',
-            },
-            animation: {
-                'spin-slower': 'spin 25s linear infinite',
-                'spin-slow': 'spin 10s linear infinite',
-            },
-            fontFamily: {
-                sans: ['Mona Sans', ...defaultTheme.fontFamily.sans],
-                display: [
-                    ['Mona Sans', ...defaultTheme.fontFamily.sans],
-                    {fontVariationSettings: '"wdth" 125'},
-                ],
-            },
-        },
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    fontSize: {
+      xs: ['0.75rem', { lineHeight: '1rem' }],
+      sm: ['0.875rem', { lineHeight: '1.5rem' }],
+      base: ['1rem', { lineHeight: '1.75rem' }],
+      lg: ['1.125rem', { lineHeight: '1.75rem' }],
+      xl: ['1.25rem', { lineHeight: '2rem' }],
+      '2xl': ['1.5rem', { lineHeight: '2.25rem' }],
+      '3xl': ['1.75rem', { lineHeight: '2.25rem' }],
+      '4xl': ['2rem', { lineHeight: '2.5rem' }],
+      '5xl': ['2.5rem', { lineHeight: '3rem' }],
+      '6xl': ['3rem', { lineHeight: '3.5rem' }],
+      '7xl': ['4rem', { lineHeight: '4.5rem' }],
     },
-    plugins: [],
+    extend: {
+      borderRadius: {
+        '4xl': '2.5rem',
+      },
+      animation: {
+        'spin-slower': 'spin 25s linear infinite',
+        'spin-slow': 'spin 10s linear infinite',
+      },
+      fontFamily: {
+        sans: ['Mona Sans', ...defaultTheme.fontFamily.sans],
+        display: [
+          ['Mona Sans', ...defaultTheme.fontFamily.sans],
+          { fontVariationSettings: '"wdth" 125' },
+        ],
+      },
+    },
+  },
+  plugins: [],
 }
