b911f45ac9b7f54706311f9539e7ee5d53a74bc8
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2025-10-30 14:14:18 +0900

    feat: remove docs button

--- components/RootLayout.tsx
+++ components/RootLayout.tsx
@@ -19,30 +19,30 @@ function Header() {
           <Image src={logo} alt={'logo NextAI'} width={80} height={80} />
         </Link>
         <div className="flex items-center justify-center gap-3">
-          <a
-            className="inline-flex items-center gap-1.5 rounded-full bg-neutral-950 px-6 py-1.5 text-sm font-semibold text-white transition hover:bg-neutral-800 sm:text-base"
-            href="https://nextjs.org/docs"
-            rel={'noreferrer'}
-            target={'_blank'}
-          >
-            <span className="relative">Docs</span>
-            <span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="h-4 w-4"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
-                />
-              </svg>
-            </span>
-          </a>
+          {/*<a*/}
+          {/*  className="inline-flex items-center gap-1.5 rounded-full bg-neutral-950 px-6 py-1.5 text-sm font-semibold text-white transition hover:bg-neutral-800 sm:text-base"*/}
+          {/*  href="https://nextjs.org/docs"*/}
+          {/*  rel={'noreferrer'}*/}
+          {/*  target={'_blank'}*/}
+          {/*>*/}
+          {/*  <span className="relative">Docs</span>*/}
+          {/*  <span>*/}
+          {/*    <svg*/}
+          {/*      xmlns="http://www.w3.org/2000/svg"*/}
+          {/*      fill="none"*/}
+          {/*      viewBox="0 0 24 24"*/}
+          {/*      strokeWidth="1.5"*/}
+          {/*      stroke="currentColor"*/}
+          {/*      className="h-4 w-4"*/}
+          {/*    >*/}
+          {/*      <path*/}
+          {/*        strokeLinecap="round"*/}
+          {/*        strokeLinejoin="round"*/}
+          {/*        d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"*/}
+          {/*      />*/}
+          {/*    </svg>*/}
+          {/*  </span>*/}
+          {/*</a>*/}
 
           <a
             target="_blank"
